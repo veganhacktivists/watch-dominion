b3c97d31fb077b78253b7a4f9ca462f15b35ba06
Stephan de Vries <stephan281094@gmail.com>
2022-05-06 22:10:41 -0500

    Make sure SEO is complete

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.svg" />
 
-    <title inertia>{{ config('app.name', 'Watch Dominion') }}</title>
+    <title inertia>Watch the award-winning and life changing documentary, Dominion!</title>
+    <meta
+      name="description"
+      content="Dominion uses drones, hidden and handheld cameras to expose the dark underbelly of modern animal agriculture, questioning the morality and validity of humankind's dominion over the animal kingdom."
+    />
+    <meta property="og:title" content="Watch Dominion" />
+    <meta
+      property="og:description"
+      content="Watch the award-winning and life changing documentary, Dominion!"
+    />
+    <meta property="og:url" content="{{ url('/') }}" />
+
+    <meta property="og:type" content="video.movie" />
+    <meta property="og:image" content="{{ url('/img/watchdominion.jpg') }}" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="384" />
+    <meta property="og:image:height" content="384" />
+    <meta
+      property="og:image:alt"
+      content="WatchDominion.org with a pig in the background, looking at the camera"
+    />
+
+    <meta property="video:director" content="Chris Delforce" />
+    <meta property="video:writer" content="Chris Delforce" />
+    <meta property="video:duration" content="7199" />
+    <meta property="video:release_date" content="2018" />
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
