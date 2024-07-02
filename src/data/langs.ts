714e1058edfd03d4d64df1933f8b1f68e5fe0bc5
Jérémy Touati <sielver@gmail.com>
2024-07-02 11:33:39 +0200

    Maintain default inference

--- src/data/langs.ts
+++ src/data/langs.ts
@@ -1,9 +1,9 @@
 import { Lang } from "@/types/lang.ts";
 
-export const langs: Record<Lang, string> = {
-  'en': 'English',
-  'it': 'Italian',
-};
+export const langs = {
+  en: 'English',
+  it: 'Italian',
+} as const satisfies Record<Lang, string>;
 
 export function isLangSupported(lang: string): lang is Lang {
   return lang in langs;
