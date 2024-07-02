515d5c596faf5682858f97c6a348d61d2f58165d
Jérémy Touati <sielver@gmail.com>
2024-07-02 12:07:01 +0200

    Avoid repeating language keys

--- src/data/langs.ts
+++ src/data/langs.ts
@@ -3,7 +3,7 @@ import { Lang } from "@/types/lang.ts";
 export const langs = {
   en: 'English',
   it: 'Italian',
-} as const satisfies Record<Lang, string>;
+} as const satisfies Record<string, string>;
 
 export function isLangSupported(lang: string): lang is Lang {
   return lang in langs;
--- src/types/lang.ts
+++ src/types/lang.ts
@@ -1,1 +1,3 @@
-export type Lang = 'en' | 'it';
+import { langs } from "@/data/langs.ts";
+
+export type Lang = keyof typeof langs;
